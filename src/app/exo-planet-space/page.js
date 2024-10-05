99bfa75a912e3405cd7469a44d430733c7628068
ManojaD2004 <manojadkc2004@gmail.com>
2024-10-05 16:22:47 +0530

    Checkpoint

--- src/app/exo-planet-space/page.js
+++ src/app/exo-planet-space/page.js
@@ -1,6 +1,12 @@
 "use client";
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { Canvas, useFrame, extend, useLoader } from "@react-three/fiber";
+import {
+  Canvas,
+  useFrame,
+  extend,
+  useLoader,
+  useThree,
+} from "@react-three/fiber";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import {
   BakeShadows,
@@ -15,35 +21,36 @@ import * as THREE from "three";
 import { UnrealBloomPass } from "three-stdlib";
 extend({ UnrealBloomPass });
 
-const SPACE_SIZE = 0.3;
+const SPACE_SIZE = 3;
+const ORBIT_TO_SUN = 0.003;
 
-function BigSphereObj({ position, scaleRatio }) {
+function BigSphereObj({ position, scaleRatio, sunRef }) {
   const meshRef = useRef();
   const colorMap = useLoader(TextureLoader, "/texture/solar/sun/2k_sun.jpg");
   useFrame((state, delta) => {
     meshRef.current.rotation.y += delta * 0.3;
   });
   return (
-    <mesh
-      position={position}
-      rotation={[0, 0, 0]}
-      ref={meshRef}
-    >
-      <perspectiveCamera  />
-      <pointLight
-        position={[0, 0, 0]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI * 2}
-      />
-      <sphereGeometry args={[SPACE_SIZE * scaleRatio, 64, 64]} />
-      <meshStandardMaterial
-        map={colorMap}
-        emissive="orange"
-        emissiveIntensity={2}
-        toneMapped={false}
-      />
+    <mesh ref={sunRef}>
+      <mesh position={position} rotation={[0, 0, 0]} ref={meshRef}>
+        <perspectiveCamera />
+        <pointLight
+          position={[0, 0, 0]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI * 2}
+        />
+        <sphereGeometry
+          args={[SPACE_SIZE * ORBIT_TO_SUN * scaleRatio, 64, 64]}
+        />
+        <meshStandardMaterial
+          map={colorMap}
+          emissive="orange"
+          emissiveIntensity={2}
+          toneMapped={false}
+        />
+      </mesh>
     </mesh>
   );
 }
@@ -52,6 +59,8 @@ function SmallSphereObj({
   plaName = "Earth",
   colorMapLoc = "/texture/solar/earth/2k_earth_daymap.jpg",
   scaleRatio,
+  planetRef,
+  changeViewPer,
 }) {
   const meshRef1 = useRef();
   const [colorMap, displacementMap, normalMap] = useLoader(TextureLoader, [
@@ -65,7 +74,7 @@ function SmallSphereObj({
   });
   return (
     <group
-      scale={hovered ? 3 : 1}
+      // scale={hovered ? 3 : 1}
       onPointerOver={(event) => {
         setHover(true);
       }}
@@ -73,7 +82,12 @@ function SmallSphereObj({
         setHover(false);
       }}
     >
-      <mesh rotation={[-Math.PI / 10, 0, 0]}>
+      {changeViewPer === true && (
+        <Html>
+          <div className="text-white text-lg select-none">I am here</div>
+        </Html>
+      )}
+      <mesh rotation={[-Math.PI / 10, 0, 0]} ref={planetRef}>
         <mesh ref={meshRef1} rotation={[-Math.PI / 2, 0, 0]}>
           <sphereGeometry args={[SPACE_SIZE * scaleRatio, 64, 64]} />
           <meshStandardMaterial
@@ -96,12 +110,13 @@ function OrbitComponent({
   speedPla,
   scaleRatio,
   planetRef,
+  changeViewPer,
 }) {
   const ellipseCurve = new THREE.EllipseCurve(
     0,
     0,
-    xRadius * SPACE_SIZE,
-    yRadius * SPACE_SIZE,
+    xRadius * SPACE_SIZE * ORBIT_TO_SUN,
+    yRadius * SPACE_SIZE * ORBIT_TO_SUN,
     0,
     2 * Math.PI,
     false,
@@ -110,7 +125,7 @@ function OrbitComponent({
   const eliRef = useRef();
   const boxRef = useRef();
 
-  const points = ellipseCurve.getPoints(100);
+  const points = ellipseCurve.getPoints(500);
   useFrame((state, delta) => {
     const newP = ellipseCurve.getPoint(
       window.performance.now() * 0.0003 * speedPla + 0.1
@@ -125,6 +140,7 @@ function OrbitComponent({
           planetRef={planetRef}
           scaleRatio={scaleRatio}
           colorMapLoc={colorMapLoc}
+          changeViewPer={changeViewPer}
         />
       </mesh>
       <mesh>
@@ -140,37 +156,139 @@ function OrbitComponent({
   );
 }
 
-export default function Home() {
+function ThreeDComp({ changeViewPer }) {
   const planetRef = useRef();
+  const cameraRef = useRef();
+  const controlsRef = useRef();
+  const sunRef = useRef();
+  const distanceCamRef = useRef(16);
+  const earthOrbit = 23479.8304;
+  useThree(({ camera }) => {
+    cameraRef.current = camera;
+  });
+  useEffect(() => {
+    if (
+      changeViewPer === false &&
+      planetRef.current &&
+      cameraRef.current &&
+      controlsRef.current
+    ) {
+      const target = new THREE.Vector3();
+      planetRef.current.getWorldPosition(target);
+      const cameraDistance = 16;
+      const cameraOffset = new THREE.Vector3(cameraDistance, 0, 0);
+      cameraRef.current.position.copy(target.clone().add(cameraOffset));
+      cameraRef.current.lookAt(target);
+      cameraRef.current.rotation.set(0, Math.PI / 2, 0);
+    } else if (
+      changeViewPer === true &&
+      sunRef.current &&
+      cameraRef.current &&
+      controlsRef.current
+    ) {
+      const target = new THREE.Vector3();
+
+      sunRef.current.getWorldPosition(target);
+      const cameraDistance = 5;
+      const cameraOffset = new THREE.Vector3(cameraDistance, 0, 0);
+      cameraRef.current.position.copy(target.clone().add(cameraOffset));
+      cameraRef.current.lookAt(target);
+      cameraRef.current.rotation.set(0, -Math.PI / 2, 0);
+    }
+  }, [changeViewPer]);
+
+  useFrame((state, delta) => {
+    if (changeViewPer === false && controlsRef.current && cameraRef.current) {
+      const target = controlsRef.current.target;
+      const distance = cameraRef.current.position.distanceTo(target);
+      console.log("Current zoom (distance to target):", distance);
+      distanceCamRef.current = distance;
+      planetRef.current.scale.set(
+        50 / distanceCamRef.current,
+        50 / distanceCamRef.current,
+        50 / distanceCamRef.current
+      );
+    }
+    if (
+      changeViewPer === false &&
+      planetRef.current &&
+      cameraRef.current &&
+      controlsRef.current
+    ) {
+      const target = new THREE.Vector3();
+      planetRef.current.getWorldPosition(target);
+      controlsRef.current.target.copy(target);
+      controlsRef.current.update();
+    } else if (
+      changeViewPer === true &&
+      sunRef.current &&
+      cameraRef.current &&
+      controlsRef.current
+    ) {
+      const target = new THREE.Vector3();
+      sunRef.current.getWorldPosition(target);
+      controlsRef.current.target.copy(target);
+      controlsRef.current.update();
+    }
+  });
   return (
-    <main className="h-screen m-[unset] bg-slate-950">
+    <>
+      <OrbitControls
+        ref={controlsRef}
+        // enableZoom={changeViewPer}
+        panSpeed={2}
+        zoomToCursor={true}
+        zoomSpeed={5}
+        rotateSpeed={2}
+      />
+      <ambientLight intensity={Math.PI / 2} />
+      <Stars
+        radius={50 * SPACE_SIZE}
+        count={10000}
+        depth={600}
+        factor={20}
+        // saturation={100}
+        fade={true}
+        speed={1}
+      />
+      <Effects disableGamma>
+        <unrealBloomPass threshold={1} strength={1.0} radius={0.5} />
+      </Effects>
+      <BakeShadows />
+      <BigSphereObj
+        sunRef={sunRef}
+        scaleRatio={109.2983}
+        position={[0, 0, 0]}
+      />
+      <OrbitComponent
+        xRadius={earthOrbit}
+        yRadius={earthOrbit}
+        scaleRatio={0.1}
+        colorMapLoc={"/texture/solar/earth/2k_earth_daymap.jpg"}
+        speedPla={0.001}
+        planetRef={planetRef}
+        changeViewPer={changeViewPer}
+      />
+    </>
+  );
+}
+
+export default function Home() {
+  const [changeView, setChangeView] = useState(true);
+  return (
+    <main className="h-screen m-[unset] relative bg-slate-950">
+      <div className="z-50 fixed top-0 right-0 m-3 rounded-xl h-40 w-44 bg-red-700 p-4 text-white">
+        <div
+          className="cursor-pointer"
+          onClick={() => setChangeView(!changeView)}
+        >
+          Change The View
+        </div>
+      </div>
       <Canvas
         camera={{ position: [240, 120, 120], fov: 50, far: 100000, near: 1 }}
       >
-        <OrbitControls />
-        <ambientLight intensity={Math.PI / 2} />
-        {/* <Stars
-          radius={500}
-          count={50000}
-          depth={600}
-          factor={20}
-          // saturation={100}
-          fade={true}
-          speed={1}
-        /> */}
-        <Effects disableGamma>
-          <unrealBloomPass threshold={1} strength={1.0} radius={0.5} />
-        </Effects>
-        <BakeShadows />
-        <BigSphereObj scaleRatio={109.2983} position={[0, 0, 0]} />
-        <OrbitComponent
-          xRadius={23479.8304}
-          yRadius={23479.8304}
-          scaleRatio={1}
-          colorMapLoc={"/texture/solar/earth/2k_earth_daymap.jpg"}
-          speedPla={0.01}
-          planetRef={planetRef}
-        />
+        <ThreeDComp changeViewPer={changeView} />
       </Canvas>
     </main>
   );
