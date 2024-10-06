918a40f9f81720c4313dd7f7660063dec56e94cc
Vilas-cp <vilaspgowda1000@gmail.com>
2024-10-06 12:53:02 +0530

    minor changes

--- src/app/helpers/getRandomColor.js
+++ src/app/helpers/getRandomColor.js
@@ -1,7 +1,7 @@
 function getRandomColor() {
   var letters = "0123456789ABCDEF";
   var color = "#";
-  for (var i = 0; i < 6; i++) {
+  for (var j = 0; j < 6; j++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
